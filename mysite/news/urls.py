3e692254e3aa95ef3f2244c27636616310e8df52
alexandr <lexspb19@gmail.com>
2022-12-08 16:37:20 +0400

    functions replaced with classes

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', HomeNews.as_view(), name='home'),
-    path('new', add, name='new'),
+    path('new', CreateNews.as_view(), name='new'),
     path('category/<int:category_id>/', NewsByCategory.as_view(), name='category'),
     path('news/<int:pk>/', ViewNews.as_view(), name='view_news')
 ]
