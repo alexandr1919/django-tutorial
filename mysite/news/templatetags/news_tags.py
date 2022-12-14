06f11ff0fc5cb18479c1048049116b95cdc34505
alexandr <lexspb19@gmail.com>
2022-12-14 14:03:10 +0400

    news counter with aggregate function, few restylings

--- mysite/news/templatetags/news_tags.py
+++ mysite/news/templatetags/news_tags.py
@@ -1,12 +1,13 @@
 from django import template
 from news.models import Category, News
+from django.db.models import Count
 
 register = template.Library()
 
 
 @register.inclusion_tag('news/list_categories.html')
 def show_categories():
-    categories = Category.objects.all()
+    categories = Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
     return {"categories": categories}
 
 @register.inclusion_tag('news/news_component.html')
